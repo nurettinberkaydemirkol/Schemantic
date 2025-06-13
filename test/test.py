8b8bfab9d463b8034b055258076c1a38606fa947
Berkay Demirkol <90573081+nurettinberkaydemirkol@users.noreply.github.com>
2025-06-14 00:57:39 +0300

    upgrade test

--- test/test.py
+++ test/test.py
@@ -1,15 +1,14 @@
 from schemantic import VectorCube 
 
 data = [
-    (0, [0.1] * 32, "ilk belge"),
-    (1, [0.9] * 32, "ikinci belge"),
-    (2, [0.2, 0.1] * 16, "üçüncü belge"),
+    (0, [0.1] * 32, "first"),
+    (1, [0.9] * 32, "second"),
+    (2, [0.2, 0.1] * 16, "third"),
 ]
 
-cube = VectorCube(data)
-
-query = [0.87] * 32
-
+cube = VectorCube(data, cluster_type="knn")  
+query = [0.11] * 32
 results = cube.query(query)
 
+# first
 print(results)
